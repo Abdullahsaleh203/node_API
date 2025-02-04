6432562facebed3f9ed79f98c3748556b9d35ffc
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2025-02-04 15:10:00 +0200

    add bcryptjs for password hashing and enhance registration validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,62 @@
 const router = require('express').Router();
 const User = require('../model/user');
-
+const bcrypt = require('bcryptjs');
 
 // VALIDATION
 const Joi = require('joi');
 
+const schema = Joi.object({
+    name : Joi.string()
+    .min(6)
+    .required(),
+    email : Joi.string()
+    .min(6).required()
+    .email(),
+    password : Joi.string()
+    .min(6).
+    required()
+})
 
 router.post('/register',async (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-    try {
-        const savedUser = await user.save();
-        res.send(savedUser);
-    } catch (err) {
-        res.status(400).send(err);
-    }
+    // Validate the data before we make a user
+    // const validation = schema.validate(req.body, schema);
+    // res.send(validation);
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Check if the user is already in the database
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send('Email already exists');
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        // Create a new user
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        try {
+            const savedUser = await user.save();
+            res.send(savedUser);
+        } catch (err) {
+            res.status(400).send(err);
+        }
+
 });
 
+// const user = new User({
+//     name: req.body.name,
+//     email: req.body.email,
+//     password: req.body.password
+// })
+// try {
+//     const savedUser = await user.save();
+//     res.send(savedUser);
+// } catch (err) {
+//     res.status(400).send(err);
+// }
 
 router.post('/login', (req, res) => {
     res.send('Login');
@@ -63,3 +100,4 @@ router.post('/register', async (req, res) => {
     }
 });
 */
+

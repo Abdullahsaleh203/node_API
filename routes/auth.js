effae4a7cf8d4bec53e586ca53bbe32d3b25da31
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-02-05 21:57:20 +0200

    remove commented-out registration logic and clean up auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -46,58 +46,11 @@ router.post('/register',async (req, res) => {
 
 });
 
-// const user = new User({
-//     name: req.body.name,
-//     email: req.body.email,
-//     password: req.body.password
-// })
-// try {
-//     const savedUser = await user.save();
-//     res.send(savedUser);
-// } catch (err) {
-//     res.status(400).send(err);
-// }
+
 
 router.post('/login', (req, res) => {
     res.send('Login');
 });
 
 module.exports = router;
-/*
-
-const Joi = require('joi');
-const bcrypt = require('bcryptjs');
-
-router.post('/register', async (req, res) => {
-    // Validate user input
-    const schema = Joi.object({
-        name: Joi.string().min(3).required(),
-        email: Joi.string().min(6).required().email(),
-        password: Joi.string().min(6).required()
-    });
-    const { error } = schema.validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    // Check if the user is already in the database
-    const emailExist = await User.findOne({ email: req.body.email });
-    if (emailExist) return res.status(400).send('Email already exists');
-
-    // Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-    // Create a new user
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-    });
-    try {
-        const savedUser = await user.save();
-        res.send(savedUser);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-*/
 
